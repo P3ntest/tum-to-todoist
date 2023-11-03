54b3279cba8a2b9bc73a985da03fb9e6e78552c9
Julius van Voorden <julius@vanvoorden.dev>
2023-11-03 16:23:21 +0100

    minor

--- index.ts
+++ index.ts
@@ -1,8 +1,9 @@
 import { TodoistApi } from "@doist/todoist-api-typescript";
 import { getAllStreams } from "./live";
 import { Database } from "bun:sqlite";
+import { Cron } from "croner";
 
-const projectName = "Vorlesungen";
+const projectName = process.env.PROJECT_NAME;
 
 const api = new TodoistApi(process.env.TODOIST_API_KEY!);
 
@@ -10,7 +11,7 @@ const project = (await api.getProjects()).find(
   (project) =>
     project.name.toLowerCase() ===
     process.env.PROJECT_NAME!.toLowerCase().trim()
-);
+)!;
 
 if (!project) {
   throw new Error(`Project ${projectName} not found`);
@@ -26,37 +27,54 @@ db.query(
     );`
 ).run();
 
-console.log("Getting streams...");
-const streams = await getAllStreams();
-
-// filter out streams that are already in the database
-console.log("Filtering streams...");
-const newStreams = streams.filter(
-  (stream) =>
-    !db.query(`SELECT * FROM streams WHERE id = ?`).all(stream.id).length
-);
-
-console.log("Adding", newStreams.length, "streams...");
-const promises = newStreams.map((stream) => {
-  return (async () => {
-    const dueDate = new Date(stream.start);
-    dueDate.setHours(12, 0, 0, 0);
-
-    await api.addTask({
-      projectId: project.id,
-      content: `${stream.slug.toUpperCase()} - ${getNiceTime(dueDate)}`,
-      description: stream.url,
-      dueDate: dueDate.toISOString().split("T")[0],
-    });
-
-    console.log("Added", stream.id);
-
-    db.query(`INSERT INTO streams VALUES (?)`).run(stream.id);
-  })();
-});
-
-await Promise.all(promises);
-
 function getNiceTime(date: Date) {
   return `${date.getDate()}.${date.getMonth() + 1}.${date.getFullYear()}`;
 }
+
+async function sync() {
+  console.log("Getting streams...");
+  const streams = await getAllStreams();
+
+  // filter out streams that are already in the database
+  console.log("Filtering streams...");
+  const newStreams = streams.filter(
+    (stream) =>
+      !db.query(`SELECT * FROM streams WHERE id = ?`).all(stream.id).length
+  );
+
+  console.log("Adding", newStreams.length, "streams...");
+  const promises = newStreams.map((stream) => {
+    return (async () => {
+      const dueDate = new Date(stream.start);
+      dueDate.setHours(12, 0, 0, 0);
+
+      await api.addTask({
+        projectId: project.id,
+        content: `${stream.slug.toUpperCase()} - ${getNiceTime(dueDate)}`,
+        description: stream.url,
+        dueDate: dueDate.toISOString().split("T")[0],
+      });
+
+      console.log("Added", stream.id);
+
+      db.query(`INSERT INTO streams VALUES (?)`).run(stream.id);
+    })();
+  });
+
+  await Promise.all(promises);
+  console.log("Done");
+}
+
+async function run() {
+  console.log("Running...", new Date().toISOString());
+  await sync();
+  console.log(
+    `Next run in ${new Date(
+      Cron(process.env.CRON!).nextRun()!.getTime() - Date.now()
+    ).getMinutes()} minutes`
+  );
+}
+
+await run();
+
+Cron(process.env.CRON!, run);
